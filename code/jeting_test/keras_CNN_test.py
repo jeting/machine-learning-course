b664e6693d973665ce5833a96daab60dde2c0581
jeting <jetingwu@gmail.com>
2019-06-04 13:44:48 +0800

    test_0604

--- code/jeting_test/keras_CNN_test.py
+++ code/jeting_test/keras_CNN_test.py
@@ -12,32 +12,53 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 x_train = mnist.train.images
 y_train = mnist.train.labels
 
-# initializing CNN
-model = Sequential()  
-model.add(Conv2D(32, 3, 3, input_shape = (1,28, 28), activation = 'relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-
-# Second convolutional layer
-model.add(Conv2D(32, 3, 3, activation = 'relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-
-# Third convolutional layer
-model.add(Conv2D(64, 3, 3, activation = 'relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-
-model.add(Dense(output_dim = 128, activation = 'relu'))
-model.add(Dense(output_dim = 1, activation = 'sigmoid'))
-
-# set cost function
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+x_test = mnist.test.images
+y_test = mnist.test.labels
 
 
-# training
-# batch_size indicates how many training data is used in each batch
-print('Start training')
-model.fit(x_train, y_train, batch_size = 100, nb_epoch = 20)
-model.save('my_model.h5')
-print('End training')
+print(x_train.shape)
+print(y_train.shape)
 
+print(x_test.shape)
+print(y_test.shape)
+
+## Initializing CNN
+#model = Sequential()  
+#model.add(Conv2D(32, 3, 3, input_shape = (128, 128, 3), activation = 'relu'))
+#model.add(MaxPooling2D(pool_size = (2, 2)))
+#
+## Second convolutional layer
+#model.add(Conv2D(32, 3, 3, activation = 'relu'))
+#model.add(MaxPooling2D(pool_size = (2, 2)))
+#
+## Third convolutional layer
+#model.add(Conv2D(64, 3, 3, activation = 'relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#
+#model.add(Flatten())
+#
+#model.add(Dense(output_dim = 128, activation = 'relu'))
+#model.add(Dense(output_dim = 1, activation = 'sigmoid'))
+#
+## Set cost function
+#model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#training_set = train_datagen.flow_from_directory('dataset/training_set', target_size = (128, 128), batch_size = 32, class_mode = 'binary')
+#test_set = test_datagen.flow_from_directory('dataset/test_set', target_size = (128, 128), batch_size = 32, class_mode = 'binary')
+#model.fit_generator(training_set, samples_per_epoch = 400, nb_epoch = 30, validation_data = test_set, nb_val_samples = 100)
+#
+#import numpy as np
+#from keras.preprocessing import image
+#
+#test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (128, 128))
+#test_image = image.img_to_array(test_image)
+#test_image = np.expand_dims(test_image, axis = 0)
+#result = model.predict(test_image)
+#training_set.class_indices
+#if result[0][0] == 1:
+#  prediction = 'dog'
+#else:
+#  prediction = 'cat'
+  
+  
+  
